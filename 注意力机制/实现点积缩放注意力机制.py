09d2ac281beb4638153a2fd5c2b828fe7aaac9ee
HAO <147186777+KARENLHAO@users.noreply.github.com>
2025-06-11 02:01:18 +0800

    add 注意力机制

---
+++ 注意力机制/实现点积缩放注意力机制.py
@@ -0,0 +1,41 @@
+import math
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+def masked_softmax(X, valid_lens):
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = valid_lens.repeat_interleave(shape[1])
+        else:
+            valid_lens = valid_lens.reshape(-1)
+        X = d2l.sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+class DotProductAttention(nn.Module):
+    def __init__(self, dropout, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens=None):
+        d = queries.shape[-1]
+        scores = torch.bmm(queries, keys.transpose(1,2)) / math.sqrt(d)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+# 示例数据
+queries = torch.normal(0, 1, (2, 1, 2))
+keys = torch.normal(0, 1, (2, 10, 2))
+values = torch.normal(0, 1, (2, 10, 4))
+valid_lens = torch.tensor([2, 6])
+
+attention = DotProductAttention(dropout=0.5)
+attention.eval()
+output = attention(queries, keys, values, valid_lens)
+
+# 可视化注意力权重
+d2l.show_heatmaps(attention.attention_weights.reshape((1, 1, 2, 10)),
+                  xlabel='Keys', ylabel='Queries')
